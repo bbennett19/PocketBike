883e82df7de2346e01ce64a46decad0f4bce61c8
Ben <benbennett1991@gmail.com>
2018-02-27 22:46:23 -0800

    Moved physics stuff to FixedUpdate

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float rotationSpeed;
     private Rigidbody rb;
+    private Vector3 lr_force = Vector3.zero;
+    private Vector3 torque = Vector3.zero;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
@@ -12,20 +14,28 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        lr_force = Vector3.zero;
+        torque = Vector3.zero;
 
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddTorque(Vector3.forward * 200f);
+            torque = Vector3.forward * 250f;
             //transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime);
         }
         if(Input.GetKey(KeyCode.D))
         {
-            rb.AddTorque(Vector3.back * 200f);
+            torque = Vector3.back * 250f;
             //transform.Rotate(Vector3.forward * rotationSpeed * -Time.deltaTime);
         }
         if(Input.GetKey(KeyCode.Space) || Input.touchCount > 0)
         {
-            rb.AddForce(Vector3.right * 250f);
+            lr_force = Vector3.right * 300f;
         }
 	}
+
+    private void FixedUpdate()
+    {
+        rb.AddTorque(torque);
+        rb.AddForce(lr_force);
+    }
 }
